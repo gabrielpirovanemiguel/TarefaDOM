96c454dd00b3b121f3853268f484b27ec7245eaa
gabrielpirovanemiguel <gabriel.balique@gmail.com>
2026-07-12 01:10:59 -0300

    refac: criando uma função de alternar classes modal

--- funcoes.js
+++ funcoes.js
@@ -1,19 +1,20 @@
-import {htmlModal,
-        htmlListaVazia
+import {
+    htmlModal,
+    htmlListaVazia
 } from "./elementos_html.js";
 
 export function ativarModalAdicionar(event) {
-    const listaContainer = document.querySelector('.lista');
-    const conteudoLista = document.querySelector('#conteudo-lista');
-    conteudoLista.classList.add('overlay');
-    listaContainer.innerHTML = htmlModal;
-    listaContainer.classList.add('modal-adicionar');
+    alternarClasessModal().innerHTML = htmlModal;
 }
 
 export function desativarModalAdicionar(event) {
+    alternarClasessModal().innerHTML = htmlListaVazia;
+}
+
+function alternarClasessModal() {
     const listaContainer = document.querySelector('.lista');
     const conteudoLista = document.querySelector('#conteudo-lista');
-    listaContainer.innerHTML = htmlListaVazia;
-    listaContainer.classList.remove('modal-adicionar');
-    conteudoLista.classList.remove('overlay');
+    listaContainer.classList.toggle('modal-adicionar');
+    conteudoLista.classList.toggle('overlay');
+    return listaContainer;
 }
